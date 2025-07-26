1650ad0faa58a1facc9cbf814cf4d415d9005c0b
Gaurav kuntal <139991052+kuntalgaurav2001@users.noreply.github.com>
2025-07-26 11:25:07 +0530

    Update chemical_inventory.py

--- backend/app/schema/chemical_inventory.py
+++ backend/app/schema/chemical_inventory.py
@@ -14,6 +14,7 @@ class ChemicalInventoryBase(BaseModel):
     supplier: Optional[str] = None
     location: Optional[str] = None
 
+
 # Create schema
 class ChemicalInventoryCreate(ChemicalInventoryBase):
     pass
